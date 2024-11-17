f2283377181544f77ed2de92f0e520e9dd27ee2c
C.S. Ning <30892154+csning1998@users.noreply.github.com>
2024-11-17 17:54:37 +0800

    Removed depends on docker-compose.yml and added port setting

--- src/db.ts
+++ src/db.ts
@@ -2,8 +2,17 @@ import {
   Sequelize
 } from "sequelize";
 
-const sequelize = new Sequelize(
-  `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@postgres:5432/${process.env.POSTGRES_DB}`
-); 
+// const sequelize = new Sequelize(
+//   `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@postgres:5432/${process.env.POSTGRES_DB}`
+// ); 
 
-export default sequelize
+
+export default new Sequelize({
+  dialect: "postgres",
+  host: "postgres",
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  // logging: false,
+  // models: [Dog],
+})
