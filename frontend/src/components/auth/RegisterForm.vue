ff07d2d69da35232d7045dc77665b2b1f0f2e542
ChingSan Ning <january20070120@gmail.com>
2024-11-23 21:11:44 +0800

    Fixed: CSS Layout

--- frontend/src/components/auth/RegisterForm.vue
+++ frontend/src/components/auth/RegisterForm.vue
@@ -8,72 +8,97 @@ const props = defineProps<{
 
 <template>
   <div class="form-container">
-    <div class="form-card">
-      <h2 class="form-title">Registration</h2>
+    <section class="form-section">
+      <div class="form-card">
+        <h2 class="form-title">Registration</h2>
 
-      <form class="form-body" @submit.prevent="onSubmit">
-        <div class="form-field">
-          <label for="userId">User ID</label>
-          <input
-            id="userId"
-            type="text"
-            v-model="registrationFormData.userId"
-            required
-          />
-        </div>
-        <div class="form-field">
-          <label for="userEmail">Email</label>
-          <input
-            id="userEmail"
-            type="email"
-            v-model="registrationFormData.userEmail"
-            required
-          />
-        </div>
-        <div class="form-field">
-          <label for="userEmail">Family Name</label>
-          <input
-            id="userFamilyName"
-            type="text"
-            v-model="registrationFormData.userFamilyName"
-            required
-          />
-          <label for="userEmail">Given Name</label>
-          <input
-            id="userGivenName"
-            type="text"
-            v-model="registrationFormData.userGivenName"
-            required
-          />
-        </div>
-        <div class="form-field">
-          <label for="userPassword">Password</label>
-          <input
-            id="userPassword"
-            type="password"
-            v-model="registrationFormData.userPassword"
-            required
-          />
-        </div>
+        <form class="form-body" @submit.prevent="onSubmit">
+          <div class="form-field">
+            <div class="form-item">
+              <label for="userId">User ID</label>
+              <input
+                id="userId"
+                type="text"
+                v-model="registrationFormData.userId"
+                required
+              />
+            </div>
+          </div>
+          <div class="form-field">
+            <div class="form-item">
+              <label for="userEmail">Email</label>
+              <input
+                id="userEmail"
+                type="email"
+                v-model="registrationFormData.userEmail"
+                required
+              />
+            </div>
+          </div>
+          <!-- <div class="form-field">
+            <div class="form-item">
+              <label class="form-field label" for="userEmail"
+                >Family Name</label
+              >
+              <input
+                id="userFamilyName"
+                type="text"
+                v-model="registrationFormData.userFamilyName"
+                required
+              />
+              <label class="form-field label" for="userEmail">Given Name</label>
+              <input
+                id="userGivenName"
+                type="text"
+                v-model="registrationFormData.userGivenName"
+                required
+              />
+            </div>
+          </div> -->
+          <div class="form-field">
+            <div class="form-item">
+              <label for="userPassword"
+                >Password</label
+              >
+              <input
+                id="userPassword"
+                type="password"
+                v-model="registrationFormData.userPassword"
+                required
+              />
+            </div>
+          </div>
+          <div class="form-field">
+            <div class="form-item">
+              <label class="label" for="confirmPassword"
+                >Confirm Password</label
+              >
+              <input
+                id="confirmPassword"
+                type="password"
+                v-model="registrationFormData.confirmPassword"
+                required
+              />
+            </div>
+          </div>
 
-        <div class="form-field">
-          <label for="confirmPassword">Confirm Password</label>
-          <input
-            id="confirmPassword"
-            type="password"
-            v-model="registrationFormData.confirmPassword"
-            required
-          />
-        </div>
-        <div class="form-button-container">
-          <button class="form-button" type="submit">Register</button>
-        </div>
-      </form>
-      <p class="signup-link">
-        Already Registered？
-        <router-link to="/login">Login</router-link>
-      </p>
-    </div>
+          <!-- <div class="form-field">
+            <div class="form-item">
+              <label class="label">&nbsp;aaa</label>
+              <button class="form-button" type="submit">Register</button>
+            </div>
+          </div> -->
+
+          <div class="form-button-container">
+            <button class="form-button" type="submit">Register</button>
+          </div>
+        </form>
+        <p class="signup-link">
+          Already Registered？
+          <router-link to="/login">Login</router-link>
+        </p>
+      </div>
+    </section>
   </div>
 </template>
 
