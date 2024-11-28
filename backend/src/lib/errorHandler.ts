414d6148dc08d725b790e99c6dde46d9ce82344b
ChingSan Ning <january20070120@gmail.com>
2024-11-28 22:37:27 +0800

    Featured: Configured prettier with .editconfig placed in the root dir. See the commend in the file for more details.

--- backend/src/lib/errorHandler.ts
+++ backend/src/lib/errorHandler.ts
@@ -1,26 +1,41 @@
-import { Express } from "express";
+import { Express, NextFunction, Request, Response } from "express";
 import type { ErrorRequestHandler } from "express";
 
 // const app: Express = express();
 // app.use(errorHandler);
 
+export const HTTPJsonResponse = function (
+    res: Response,
+    status: any,
+    payload: any,
+): Response<any, Record<string, any>> {
+    return res.status(status).json({
+        payload,
+        ...status,
+    });
+};
 
-module.exports = function(app: Express) {
-  const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    console.error(err)
-    res.status(err.status || 500)
-  
-    if (app.get('env') === 'development') {
-      app.locals.pretty = true
-      res.json({
-        payload: err.message
-      })
-    } else {
-      res.json({
-        payload: "Server error"
-      })
-    }
-  };
+module.exports = function (app: Express): void {
+    const errorHandler: ErrorRequestHandler = (
+        err: any,
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): void => {
+        console.error(err);
+        res.status(err.status || 500);
 
-  app.use(errorHandler);
-}
+        if (app.get("env") === "development") {
+            app.locals.pretty = true;
+            res.json({
+                payload: err.message,
+            });
+        } else {
+            res.json({
+                payload: "Server error",
+            });
+        }
+    };
+
+    app.use(errorHandler);
+};
--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,65 +1,63 @@
-import express, { Router, Request, Response } from 'express';
+import express, { Router, Request, Response } from "express";
 import * as JWTToken from "../lib/jwt-token";
 // import "dotenv.config";
 import { statusCodes } from "../lib/statusCodes";
 import ProductModel from "../mongo-models/product";
+import { HTTPJsonResponse } from "../lib/errorHandler";
 import { Schema } from "mongoose";
 import Product from "../mongo-models/product";
 
-
+/*
+ * User can view and search products without login.
+ * This route aims to retrieve all published merchandise (filtering by the state column).
+ * Assume the only query strings we would receive is "Keyword"
+ * */
 const router: Router = express.Router();
 
-const HTTPJsonResponse = function (res: Response, status: any, payload:any) {
-    return res.status(status).json({
-        payload,
-        ...status,
-    });
-}
-
-const HTTPJsonUserErrorResponse = function (res: Response, error: Error, statusCode: 400){
-    return res.status(statusCode).json({
-        message: error.message,
-    });
-}
-
-/*
-* User can view and search products without login.
-* This route aims to retrieve all published merchandise (filtering by the state column).
-* Assume the only query strings we would receive is "Keyword"
-* */
-
-router.get('/products', async (req: Request, res: Response): Promise<void> => {
-
+router.get("/products", async (req: Request, res: Response): Promise<void> => {
     const { keyword } = req.body;
     try {
+        // https://www.mongodb.com/docs/manual/reference/operator/query/regex/
         const query = keyword
-            ? {state: true, name: { $regex: keyword , $options: "i" }}
-            : {state: true}
+            ? { state: true, name: { $regex: keyword, $options: "i" } }
+            : { state: true };
         const products = await ProductModel.find(query);
 
-        HTTPJsonResponse(res, statusCodes.QUERYING.SUCCEED_BULK.code, {products});
-
-    }  catch (err) {
+        HTTPJsonResponse(res, statusCodes.QUERYING.SUCCEED_BULK.code, {
+            products,
+        });
+    } catch (err) {
         console.error(err);
         res.status(statusCodes.BACKEND_LOGIC.code).json({
             ...statusCodes.BACKEND_LOGIC,
         });
     }
-})
-
-router.get('products/:id', async (req: Request, res: Response): Promise<any> => {
-    const { id } = req.params;
-    try {
-        const product = await ProductModel.findById(id);
-        if (!product || !product.state) {
-            return res.status(statusCodes.QUERYING.SUCCEED_UNPUBLISHED_PRODUCT.code).json({
-                ...statusCodes.QUERYING.SUCCEED_UNPUBLISHED_PRODUCT
-            });
-        }
-        return HTTPJsonResponse(res, statusCodes.QUERYING.SUCCEED_UNPUBLISHED_PRODUCT, product);
-    } catch (error) {
-        return HTTPJsonResponse(res, statusCodes.BACKEND_LOGIC, error);
-    }
 });
 
-export default router
+router.get(
+    "products/:id",
+    async (req: Request, res: Response): Promise<any> => {
+        const { id } = req.params;
+        try {
+            const product = await ProductModel.findById(id);
+            if (!product || !product.state) {
+                return res
+                    .status(
+                        statusCodes.QUERYING.SUCCEED_UNPUBLISHED_PRODUCT.code,
+                    )
+                    .json({
+                        ...statusCodes.QUERYING.SUCCEED_UNPUBLISHED_PRODUCT,
+                    });
+            }
+            return HTTPJsonResponse(
+                res,
+                statusCodes.QUERYING.SUCCEED_UNPUBLISHED_PRODUCT,
+                product,
+            );
+        } catch (error) {
+            return HTTPJsonResponse(res, statusCodes.BACKEND_LOGIC, error);
+        }
+    },
+);
+
+export default router;
