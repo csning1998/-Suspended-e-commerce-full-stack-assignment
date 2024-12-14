21e1e6920a022dd53f950a5a67ff011b635fd87d
ChingSan Ning <january20070120@gmail.com>
2024-12-15 01:27:30 +0800

    Updated: .gitignore for deployment scripts

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Front end
-cd /home/csning/Documents/GitHub/e-commerce-full-stack/frontend
-npm run build-only
-rsync -av ./dist/* e-commerce-miia.com:/var/www/e-commerce-miia.com
-cd /home/csning/Documents/GitHub/e-commerce-full-stack
-
-# Backend....
